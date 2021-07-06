7c8de1213fbb4d89b90cdae01e92aaeace39f491
meta <eigenform@users.noreply.github.com>
2021-07-06 08:16:00 -0500

    kernel: fix busy-loop for hanging NMIs

--- kernel/trampoline.c
+++ kernel/trampoline.c
@@ -9,7 +9,7 @@
 #include "msr.h"
 
 extern struct ibstrace_state state;
-int __trampoline_start(void *code_ptr);
+int __trampoline_start(void *code_ptr, u64 xloops);
 
 
 // Cursed hack #3: 
@@ -20,7 +20,15 @@ void trampoline(void *info)
 {
 	int res = -1;
 
-	res = __trampoline_start(state.code_buf);
+	// Plagarized from __const_udelay()
+	unsigned long xloops = 0x10c7 * 4;
+	unsigned long lpj = this_cpu_read(cpu_info.loops_per_jiffy);
+	int d0;
+	asm("mull %%edx"
+		:"=d" (xloops), "=&a" (d0)
+		:"1" (xloops), "0" (lpj * (HZ / 4)));
+
+	res = __trampoline_start(state.code_buf, xloops);
 	pr_info("ibstrace: trampoline returned %d\n", res);
 
 	// This lock is aquired in ibstrace_ioctl() just before we use
